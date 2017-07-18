1d214afc8c673ba8778784d177b0ece5f22c1d80
Bekzhan <bkalzhan@gmail.com>
2017-07-18 13:47:14 +0000

    Add confirm email

---
+++ db/migrate/20170718125843_add_confirmable_to_devise.rb
@@ -0,0 +1,5 @@
+class AddConfirmableToDevise < ActiveRecord::Migration
+  def change
+  end
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170718093141) do
+ActiveRecord::Schema.define(version: 20170718125843) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -72,11 +72,16 @@ ActiveRecord::Schema.define(version: 20170718093141) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
