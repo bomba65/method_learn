250f528b7172ee395607321b83b529c3ad383929
nazerket <nazerke.turtayeva@nu.edu.kz>
2017-07-19 19:59:07 +0600

    courses page ready, adapted version of lessons page has not ready yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,19 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-=======
-  
+
   get '/profile' , to: "static_pages#profile"
-  
+
   get '/lessons' , to: "lessons#index"
->>>>>>> 20cc86a34379763cdd601ec55d29c13444f323ef
 
   get '/about' , to: "static_pages#about"
-  
+
   get '/home' , to: "static_pages#home"
-  
+
   root 'static_pages#home'
-  
+
   resources :courses
   resources :lessons
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  
-end
 
+end
