17bf5110f0a788723803d789021c33ae99855313
Nurbolat Kaushev <nurbolat.kaushev@gmail.com>
2017-07-19 09:47:51 +0000

    from Nurbolat2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   
+  get '/profile' , to: "static_pages#profile"
+  
   get '/lessons' , to: "lessons#index"
 
   get '/about' , to: "static_pages#about"
+  
+  get '/home' , to: "static_pages#home"
   
   root 'static_pages#home'
   
@@ -10,4 +14,6 @@ Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  
 end
+
