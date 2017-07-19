390713978df088d105878bedc0f9ae823e173ee6
Bekzhan <bkalzhan@gmail.com>
2017-07-19 10:09:39 +0000

    Change links 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  
-  get '/lessons' , to: "lessons#index"
 
   get '/about' , to: "static_pages#about"
   
   root 'static_pages#home'
   
   resources :courses
+  resources :lessons
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
