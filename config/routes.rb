092287bc05ea04b072abf5519454733c298d5778
Bekzhan <bkalzhan@gmail.com>
2017-07-19 23:00:12 +0000

    Changes with links 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-=======
+
   
   get '/profile' , to: "static_pages#profile"
-  
-  get '/lessons' , to: "lessons#index"
->>>>>>> 20cc86a34379763cdd601ec55d29c13444f323ef
-
   get '/about' , to: "static_pages#about"
-  
   get '/home' , to: "static_pages#home"
   
   root 'static_pages#home'
@@ -17,6 +11,7 @@ Rails.application.routes.draw do
   resources :lessons
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
   devise_for :users
   
 end
