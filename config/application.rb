b92f1eb9af9c162ea8fc4e1800bf2d0fca4d9ff9
Bekzhan <bkalzhan@gmail.com>
2017-07-24 08:10:05 +0000

    Add users to courses

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Workspace
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -22,6 +23,7 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.autoload_paths += %W(#{config.root}/lib)
     
   end
 end
--- hello.py
+++ hello.py
@@ -1,2 +1,5 @@
-while(true):
-    print("loh")
+import math
+
+a = "A a a a asd  sd w"
+
+print(a.split())
