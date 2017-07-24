6778ad46cc6d7e7a41d2f622514c627e3b6a017b
Bekzhan <bkalzhan@gmail.com>
2017-07-24 13:25:55 +0000

    Fixed bugs

--- db/schema.rb
+++ db/schema.rb
@@ -56,14 +56,6 @@ ActiveRecord::Schema.define(version: 20170724092510) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "process_of_lessons", force: :cascade do |t|
-    t.integer  "user_to_course"
-    t.integer  "lesson_id"
-    t.boolean  "done"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
     t.string   "answer"
