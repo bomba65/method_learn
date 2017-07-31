ef4224261407baad4cde7c7ef488e03dbba43ecc
Ruslan Shayakhmetov <cdwrss@gmail.com>
2017-07-31 12:25:41 +0000

    loly pop sosni

--- app/controllers/process_of_lessons_controller.rb
+++ app/controllers/process_of_lessons_controller.rb
@@ -1,8 +1,9 @@
 class ProcessOfLessonsController < ApplicationController
 
     def create
-        if ProcessOfLesson.where(:user_to_course_id=> params[:user_to_course_id], :lesson_id=> params[:lesson_id]).size == 0
-            ProcessOfLesson.create(:user_to_course_id => params[:user_to_course_id], :lesson_id => params[:lesson_id])
+        if ProcessOfLesson.where(:user_to_course_id=> params[:user_to_course_id], :lesson_id=> params[:lesson_id]).size > 0
+        else
+            ProcessOfLesson.create(:user_to_course_id=> params[:user_to_course_id], :lesson_id=> params[:lesson_id])
         end
     end
     
