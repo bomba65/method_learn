63d663a04cababefebdf1b8ba578fd2bdea3a3a4
Bekzhan <bkalzhan@gmail.com>
2017-08-03 08:15:57 +0000

    Change something

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || courses_path
   end
@@ -12,7 +13,11 @@ class ApplicationController < ActionController::Base
   end
   
   def authorize_admin
-    redirect_to root_path, status: 401 unless current_user.admin
+    if current_user.present?
+      redirect_to root_path, status: 401 unless current_user.admin
+    else
+       redirect_to root_path, status: 401
+    end
     #redirects to previous page
   end
   
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,13 +6,7 @@ class StaticPagesController < ApplicationController
 
   def about
   end
-  def getconsole 
-    @console_code = "$('#console').ace({ theme: 'twilight', lang: 'ruby'});";
-    @my_hash = {:code => @console_code}
-    respond_to do |format|
-      format.json { render json: @my_hash }
-    end
-  end 
+  
   def profile
     @courses = Course.all
     @finished_courses = UserToCourse.where(user_id: current_user.id, progress: 1)
